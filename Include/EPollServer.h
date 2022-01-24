cfc6a457bd6bea7e94344cefd0b884f24b01e924
geneidy <geneidy@gatech.edu>
2022-01-24 09:46:28 -0800

    add license header

--- Include/EPollServer.h
+++ Include/EPollServer.h
@@ -1,8 +1,20 @@
-//*********************************************************************************************//
-// A LOT OF CREDIT IS DUE TO:
-//https://github.com/linfan/TCP-IP-Network-basic-examples/blob/master/chapter9/EpollServer.c
-//
-//********************************************************************************************//
+/* 
+ * This file is part of the EPollServer (https://github.com/geneidy/epollserver).
+ * Copyright (c) 2017 geneidy.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <sys/socket.h>
